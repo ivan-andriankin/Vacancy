cdec9f4100fda756379e75e7084382b399b8c6bc
ivan-andriankin <andriankin.ivan@yandex.ru>
2023-02-11 20:00:51 +0400

    minor fixes

--- src/test/java/ru/hh/Tests.java
+++ src/test/java/ru/hh/Tests.java
@@ -1,12 +1,14 @@
 package ru.hh;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 
+@Tag("vacancy_tests")
 public class Tests extends TestBase {
 
     @MethodSource("menuItems")
@@ -43,7 +45,7 @@ public class Tests extends TestBase {
                 .goToCompanyPageFromVacancyCard(x5Digital)
                 .expandVacanciesDropDownCategories("Вакансии в других регионах")
                 .expandVacanciesDropDownCategories("Тестировщик")
-                .clickOnTheVacancy("QA-инженер (web)");
+                .clickOnTheVacancy("Backend QA engineer");
     }
 
 
